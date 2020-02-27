83d65da66a42c917a21a5d2ae7ebe2da4590d9f6
Abdullah <ahamad2@unl.edu>
2020-02-26 19:06:52 -0600

    project group

---
+++ semesterProject/src/com/tbf/LoadNParse.java
@@ -0,0 +1,172 @@
+package com.tbf;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LoadNParse {
+	
+	public static List<Person> parsePersonsFile() {
+		List<Person> personList = new ArrayList<Person>();
+		Scanner inputFile = null;
+		try {
+			inputFile = new Scanner(new File("data/Persons.dat"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Person persons = new Person();
+		inputFile.nextLine();
+		while (inputFile.hasNext()) {
+			String line = inputFile.nextLine();
+			String tokens[] = line.split(";");
+			String name[] = tokens[2].split(",");
+			String address[] = tokens[3].split(",");
+			Name newName = new Name(name[1], name[0]);
+			Address newAddress = new Address(address[0], address[1], address[2], address[3], address[4]);
+			if (tokens.length == 4) {
+				persons = new Person(tokens[0], tokens[1], newName, newAddress);
+
+			} else {
+				ArrayList<String> newEmail = new ArrayList<>();
+				newEmail.add(tokens[4]);
+				persons = new Person(tokens[0], tokens[1], newName, newAddress, newEmail);
+			}
+			personList.add(persons);
+		}
+		inputFile.close();
+		return personList;
+	}
+	
+	
+	public static List<Asset> parseAssetsFile() {
+		List<Asset> assList = new ArrayList<Asset>();
+		
+		Scanner inputFile = null;
+		try {
+			inputFile = new Scanner(new File("data/Assets.dat"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		inputFile.nextLine();
+		while (inputFile.hasNextLine()) {
+			String line = inputFile.nextLine();
+			String tokens[] = line.split(";");
+			Asset newAsset = null;
+			if (tokens[1].contains("D")) {
+				newAsset = new DepositAsset(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]));
+			
+			} else if (tokens[1].contains("S")) {
+				newAsset = new Stock(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]),
+						Double.parseDouble(tokens[4]), Double.parseDouble(tokens[5]), tokens[6],
+						Double.parseDouble(tokens[7]));
+
+			} else if (tokens[1].contains("P")) {
+				newAsset = new PrivateInvest(tokens[0], tokens[1], tokens[2], Double.parseDouble(tokens[3]),
+						Double.parseDouble(tokens[4]), Double.parseDouble(tokens[5]), Double.parseDouble(tokens[6]));
+
+			}
+			assList.add(newAsset);
+		}
+		
+		inputFile.close();
+		return assList;
+
+	}
+	
+	public static List<Portfolio> parsePortfoliosFile() {
+		List<Portfolio> portList = new ArrayList<Portfolio>();
+		
+		
+		Scanner inputFile = null;
+		try {
+			inputFile = new Scanner(new File("data/Portfolios.dat"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		inputFile.nextLine();
+		while (inputFile.hasNextLine()) {
+			String line = inputFile.nextLine();
+			String tokens[] = line.split(";");
+			//saving the new information from the portfolio into the classes
+			String assTokens[] = tokens[4].split(",");
+			List<Asset> assList = parseAssetsFile();
+			List<Asset> newList = new ArrayList<Asset>();
+			Asset newAss = null;
+			for (Asset a : assList) {
+				for (String s : assTokens) {
+					if(s.contains(a.getCode())) {
+						String oneAsset[] = s.split(":");
+						if(a.getAccType() == "D") {
+							newAss = new DepositAsset(a.getCode(), a.getAccType(), a.getLabel(), a.getApr(), Double.parseDouble(oneAsset[1]));
+						} else if (a.getAccType() == "S") {
+							newAss = new Stock(a.getCode(), a.getAccType(), a.getLabel(), a.getQuartDivi(), a.getBaseROR(), a.getBeta(), a.getStockSymb(), 
+									a.getSharePrice(), Integer.parseInt(oneAsset[1]));
+						} else if (a.getAccType() == "P"){
+							newAss = new PrivateInvest(a.getCode(), a.getAccType(), a.getLabel(), a.getQuartDivi(), a.getBaseROR(), a.getOmega(), 
+									a.getTotalValue(), Double.parseDouble(oneAsset[1]));
+						}
+						newList.add(newAss);
+					}
+				}
+			}
+			Portfolio newPort = null;		
+			Person owner = getPerson(tokens[1]);
+			Person manag = getPerson(tokens[2]);
+			Person benef = getPerson(tokens[3]);
+			
+			if (tokens.length == 3) {
+				newPort = new Portfolio(tokens[0], owner, manag);
+
+			} else if (tokens.length == 4) {
+				newPort = new Portfolio(tokens[0], owner, manag, benef);
+			} else {
+				newPort = new Portfolio(tokens[0], owner, manag, benef, newList);
+			}
+			portList.add(newPort);
+		}
+		
+		return portList;
+	}
+	
+	public static Person getPerson(String token) {
+		Person thisPerson = new Person();
+		if (token.isEmpty() || token == null) {
+			thisPerson = null;
+		} else {
+			List<Person> persList = parsePersonsFile();
+			for (Person p : persList) {
+				if (p.getPersonCode() == token) {
+					thisPerson = p;
+				}
+			}
+		}
+		
+		return thisPerson;
+	}
+	
+//	private List<Portfolio> getPortfolioAssets(String assetToken){
+//		List<Portfolio> portAssets = parsePortfoliosFile();
+//		String assTokens[] = portAssets.getAssetList();
+//		Asset assets = new Asset();
+//		assets = new DepositAsset(assTokens[0], Double.parseDouble(assTokens[1]));
+//		//tokenized it stoke
+//		//loop trough 
+//		//token it again(:)
+//		//num value and code
+//		//find the code tokeninside asset list
+//		//if it mathches
+//		//set that specific asset's numerical value 
+//		
+//		return portAssets;
+//		
+//	}
+	
+
+}
---
+++ semesterProject/src/com/tbf/Portfolio.java
@@ -0,0 +1,178 @@
+package com.tbf;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class Portfolio {
+	private String portCode;
+	private Person ownerCode;
+	private Person managCode;
+	private Person benefCode;
+	private List<Asset> assetList;
+	
+	
+	public double getCommission() {
+		double commi = 0;
+		if (ownerCode.getBrokerStatus().contains("E")) {
+			commi = (.0375)*getReturn();
+			
+		} else if (ownerCode.getBrokerStatus().contains("J")) {
+			commi = (.0125)*getReturn();
+		}
+//		List<Person> persList = LoadNParse.parsePersonsFile();
+//		double commi = 0;
+//		for (Person i : persList) {
+//			if (i.getPersonCode() == ownerCode && i.getBrokerStatus().contains("E")) {
+//				commi = (.0375)*getReturn();
+//				
+//			} else if (i.getPersonCode() == ownerCode && i.getBrokerStatus().contains("J")) {
+//				commi = (.0125)*getReturn();
+//			}
+//		}
+		return commi;
+	}
+	
+	public HashMap<String, Integer> getBrokerAssNum() {
+		Scanner inputFile = null;
+		try {
+			inputFile = new Scanner(new File("data/Portfolios.dat"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		HashMap<String, Integer> portAssNum = new HashMap<>();
+		
+		inputFile.nextLine();
+		while (inputFile.hasNextLine()) {
+			String line = inputFile.nextLine();
+			String tokens[] = line.split(";");
+			//saving the new information from the portfolio into the classes
+			String assTokens[] = tokens[4].split(",");
+			portAssNum.put(tokens[0], assTokens.length);
+		}
+		return portAssNum;
+	}
+	
+	public double getReturn() {
+		List<Asset> list = this.assetList;
+		double theReturn = 0;
+		for (Asset a : list) {
+			if (a.getCode() == this.ownerCode) {
+				if (a.getAccType() == "D") {
+					theReturn = (Math.exp(a.getApr())-1)*a.getAmountVal();
+					
+					
+				} else if (a.getAccType() == "S") {
+					double value = a.getNumberShares()*a.getSharePrice();
+					theReturn = (a.getBaseROR()*value*+(4*(a.getQuartDivi()))*a.getNumberShares());
+					
+					
+				} else if (a.getAccType() == "P") {
+					theReturn = a.getBaseROR()*a.getTotalValue()+(4*(a.getQuartDivi()));
+				}
+			}
+		}
+		return theReturn;
+	}
+	
+	
+	public double getFee() {
+		double fee = 0;
+		if (ownerCode.getBrokerStatus().contains("E")) {
+			fee = getCommission();
+
+		} else if (ownerCode.getBrokerStatus().contains("J")) {
+			fee = 75*getBrokerAssNum().get(portCode)+getCommission();
+		}
+//		for (Person i : persList) {
+//			if (i.getPersonCode() == ownerCode && i.getBrokerStatus().contains("E")) {
+//				fee = getCommission();
+//				
+//			} else if (i.getPersonCode() == ownerCode && i.getBrokerStatus().contains("J")) {
+//				fee = 75*getBrokerAssNum().get(portCode)+getCommission();
+//			}
+//		}
+		return fee;
+	}
+	
+	public String getOwnerName() {
+		String name = null;
+		name = ownerCode.getName().getLastName() + ", " + ownerCode.getName().getLastName();
+
+		return name;
+	}
+	
+	public String getManagerName() {
+		String name = null; 
+		name = managCode.getName().getLastName() + ", " + managCode.getName().getLastName();
+//		List<Person> persList = LoadNParse.parsePersonsFile();
+//		String name = null;
+//		for (Person i : persList) {
+//			if (i.getPersonCode() == managCode) {
+//				name = i.getName().getLastName() + ", " + i.getName().getFirstName();
+//			}
+//		}
+		return name;
+	}
+	//STORE OWNER CODE AND MANGCODE AS A "PERSON"
+	public Portfolio(String portCode, Person ownerCode, Person managCode, Person benefCode, List<Asset> assetList) {
+		super();
+		this.portCode = portCode;
+		this.ownerCode = ownerCode;
+		this.managCode = managCode;
+		this.benefCode = benefCode;
+		this.assetList = assetList;
+	}
+	
+	public Portfolio(String portCode, Person ownerCode, Person managCode, Person benefCode) {
+		super();
+		this.portCode = portCode;
+		this.ownerCode = ownerCode;
+		this.managCode = managCode;
+		this.benefCode = benefCode;
+	}
+	
+	
+	public Portfolio(String portCode, Person ownerCode, Person managCode) {
+		super();
+		this.portCode = portCode;
+		this.ownerCode = ownerCode;
+		this.managCode = managCode;
+	}
+	
+
+	public String getPortCode() {
+		return portCode;
+	}
+	public void setPortCode(String portCode) {
+		this.portCode = portCode;
+	}
+	public Person getOwnerCode() {
+		return ownerCode;
+	}
+	public void setOwnerCode(Person ownerCode) {
+		this.ownerCode = ownerCode;
+	}
+	public Person getManagCode() {
+		return managCode;
+	}
+	public void setManagCode(Person managCode) {
+		this.managCode = managCode;
+	}
+	public Person getBenefCode() {
+		return benefCode;
+	}
+	public void setBenefCode(Person benefCode) {
+		this.benefCode = benefCode;
+	}
+	public List<Asset> getAssetList() {
+		return assetList;
+	}
+	public void setAssetList(List<Asset> assetList) {
+		this.assetList = assetList;
+	}
+
+}
