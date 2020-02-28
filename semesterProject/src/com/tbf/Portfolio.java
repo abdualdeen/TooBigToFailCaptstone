c56d4aacd0c4b63899db9bb8f37530dc384acb78
Abdullah <ahamad2@unl.edu>
2020-02-27 22:36:22 -0600

    new

--- semesterProject/src/com/tbf/Portfolio.java
+++ semesterProject/src/com/tbf/Portfolio.java
@@ -6,54 +6,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
-
+/**
+ * 
+ * 
+ *
+ */
 public class Portfolio {
 	private String portCode;
 	private Person ownerCode;
 	private Person managCode;
 	private Person benefCode;
 	private List<Asset> assetList;
-	
-	
+
 	public double getTotal() {
 		List<Asset> list = this.assetList;
 		double total = 0;
 		for (Asset a : list) {
 			if (a.getAccType().contains("D")) {
 				total += a.getAmountVal();
-				
+
 			} else if (a.getAccType().contains("S")) {
-				total += a.getSharePrice()*a.getNumberShares();
+				total += a.getSharePrice() * a.getNumberShares();
 
 			} else if (a.getAccType().contains("P")) {
-				total += a.getTotalValue()*a.getPercentStake();
+				total += a.getTotalValue() * a.getPercentStake();
 
 			}
 		}
 		return total;
 	}
-	
+
 	public double getCommission() {
 		double commi = 0;
 		if (managCode.getBrokerStatus().contains("E")) {
-			commi = (.0375)*getReturn();
-			
+			commi = (.0375) * getReturn();
+
 		} else if (managCode.getBrokerStatus().contains("J")) {
-			commi = (.0125)*getReturn();
+			commi = (.0125) * getReturn();
 		}
-//		List<Person> persList = LoadNParse.parsePersonsFile();
-//		double commi = 0;
-//		for (Person i : persList) {
-//			if (i.getPersonCode() == ownerCode && i.getBrokerStatus().contains("E")) {
-//				commi = (.0375)*getReturn();
-//				
-//			} else if (i.getPersonCode() == ownerCode && i.getBrokerStatus().contains("J")) {
-//				commi = (.0125)*getReturn();
-//			}
-//		}
+
 		return commi;
 	}
-	
+
 	public HashMap<String, Integer> getBrokerAssNum() {
 		Scanner inputFile = null;
 		try {
@@ -61,98 +55,88 @@ public class Portfolio {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		HashMap<String, Integer> portAssNum = new HashMap<>();
-		
+
 		inputFile.nextLine();
 		while (inputFile.hasNextLine()) {
 			String line = inputFile.nextLine();
-			String tokens[] = line.split(";",-1);
-			//saving the new information from the portfolio into the classes
+			String tokens[] = line.split(";", -1);
+			// saving the new information from the portfolio into the classes
 			String assTokens[] = tokens[4].split(",");
 			portAssNum.put(tokens[0], assTokens.length);
 		}
 		return portAssNum;
 	}
-	
+
 	public double getReturn() {
 		List<Asset> list = this.assetList;
 		double theReturn = 0;
 		for (Asset a : list) {
-				if (a.getAccType().contains("D")) {
-					theReturn += (Math.exp(a.getApr())-1)*a.getAmountVal();
-					
-					
-				} else if (a.getAccType().contains("S")) {
-					double value =a.getSharePrice();
-					theReturn += (a.getBaseROR()*value)+(4*(a.getQuartDivi()));//*a.getNumberShares();
-					
-					
-				} else if (a.getAccType().contains("P")) {
-					theReturn += (a.getBaseROR()*a.getTotalValue()+(4*(a.getQuartDivi())));//*a.getPercentStake();
-				}
+			if (a.getAccType().contains("D")) {
+				theReturn = (Math.exp(a.getApr()) - 1) * a.getAmountVal();
+
+			} else if (a.getAccType().contains("S")) {
+				double value = a.getSharePrice();
+				theReturn = (a.getBaseROR() * value + (4 * (a.getQuartDivi())));// *a.getNumberShares();
+
+			} else if (a.getAccType().contains("P")) {
+				theReturn = (a.getBaseROR() * a.getTotalValue()) + (4 * (a.getQuartDivi()));// *a.getPercentStake();
+			}
 		}
 		return theReturn;
 	}
+
 	public double getFee() {
 		double fee = 0;
 		if (ownerCode.getBrokerStatus().contains("E")) {
 			fee = 0;
 
 		} else if (ownerCode.getBrokerStatus().contains("J")) {
-			fee = (75*getBrokerAssNum().get(portCode));
+			fee = (75 * getBrokerAssNum().get(portCode));
 		}
-//		for (Person i : persList) {
-//			if (i.getPersonCode() == ownerCode && i.getBrokerStatus().contains("E")) {
-//				fee = getCommission();
-//				
-//			} else if (i.getPersonCode() == ownerCode && i.getBrokerStatus().contains("J")) {
-//				fee = 75*getBrokerAssNum().get(portCode)+getCommission();
-//			}
-//		}
+
 		return fee;
 	}
-	
+
 	public String getOwnerName() {
 		String name = null;
 		name = ownerCode.getName().getLastName() + ", " + ownerCode.getName().getFirstName();
 
 		return name;
 	}
+
 	public double getFeeTotal() {
 		double feeTotal = 0;
 		feeTotal += getFee();
 		return feeTotal;
-		
+
 	}
+
 	public double getCommiTotal() {
 		double commiTotal = 0;
 		commiTotal += getCommission();
 		return commiTotal;
 	}
+
 	public double getReturnTotal() {
 		double returnTotal = 0;
 		returnTotal += getReturn();
 		return returnTotal;
 	}
+
 	public double getTotalTotal() {
 		double totalTotal = 0;
 		totalTotal += getTotal();
 		return totalTotal;
 	}
+
 	public String getManagerName() {
-		String name = null; 
+		String name = null;
 		name = managCode.getName().getLastName() + ", " + managCode.getName().getFirstName();
-//		List<Person> persList = LoadNParse.parsePersonsFile();
-//		String name = null;
-//		for (Person i : persList) {
-//			if (i.getPersonCode() == managCode) {
-//				name = i.getName().getLastName() + ", " + i.getName().getFirstName();
-//			}
-//		}
 		return name;
 	}
-	
+
 	public double getWeightedRisk() {
 		double risk1 = 0;
 		double risk2 = 0;
@@ -163,31 +147,28 @@ public class Portfolio {
 		double v3 = 0;
 		double weightedRisk = 0;
 		List<Asset> list = this.assetList;
-		if(!list.isEmpty()) {
-			for(Asset a : list) {
+		if (!list.isEmpty()) {
+			for (Asset a : list) {
 				if (a.getAccType().contains("P")) {
-					risk1 += a.getOmega() + Math.exp(-125500/a.getTotalValue());
+					risk1 += a.getOmega() + Math.exp(-125500 / a.getTotalValue());
 					v1 += a.getTotalValue();
-				}else if (a.getAccType().contains("S")) {
+				} else if (a.getAccType().contains("S")) {
 					risk2 += a.getBeta();
-					v2 += (a.getSharePrice()*a.getNumberShares());
-				}else if (a.getAccType().contains("D")) {
+					v2 += (a.getSharePrice() * a.getNumberShares());
+				} else if (a.getAccType().contains("D")) {
 					risk3 = 0;
 					v3 += a.getAmountVal();
 				}
 			}
 			v = v1 + v2 + v3;
-		weightedRisk = risk1*(v1/v) + risk2*(v2/v) + risk3;
-		}
-		else {
+			weightedRisk = risk1 * (v1 / v) + risk2 * (v2 / v) + risk3;
+		} else {
 			weightedRisk = 0;
 		}
 		return weightedRisk;
 	}
-	
-	
-	
-	//STORE OWNER CODE AND MANGCODE AS A "PERSON"
+
+	// STORE OWNER CODE AND MANGCODE AS A "PERSON"
 	public Portfolio(String portCode, Person ownerCode, Person managCode, Person benefCode, List<Asset> assetList) {
 		super();
 		this.portCode = portCode;
@@ -196,7 +177,7 @@ public class Portfolio {
 		this.benefCode = benefCode;
 		this.assetList = assetList;
 	}
-	
+
 	public Portfolio(String portCode, Person ownerCode, Person managCode, Person benefCode) {
 		super();
 		this.portCode = portCode;
@@ -205,8 +186,7 @@ public class Portfolio {
 		this.benefCode = benefCode;
 		this.assetList = new ArrayList<Asset>();
 	}
-	
-	
+
 	public Portfolio(String portCode, Person ownerCode, Person managCode) {
 		super();
 		this.portCode = portCode;
@@ -214,35 +194,43 @@ public class Portfolio {
 		this.managCode = managCode;
 		this.assetList = new ArrayList<Asset>();
 	}
-	
 
 	public String getPortCode() {
 		return portCode;
 	}
+
 	public void setPortCode(String portCode) {
 		this.portCode = portCode;
 	}
+
 	public Person getOwnerCode() {
 		return ownerCode;
 	}
+
 	public void setOwnerCode(Person ownerCode) {
 		this.ownerCode = ownerCode;
 	}
+
 	public Person getManagCode() {
 		return managCode;
 	}
+
 	public void setManagCode(Person managCode) {
 		this.managCode = managCode;
 	}
+
 	public Person getBenefCode() {
 		return benefCode;
 	}
+
 	public void setBenefCode(Person benefCode) {
 		this.benefCode = benefCode;
 	}
+
 	public List<Asset> getAssetList() {
 		return assetList;
 	}
+
 	public void setAssetList(List<Asset> assetList) {
 		this.assetList = assetList;
 	}
--- semesterProject/src/com/tbf/PortfolioReport.java
+++ semesterProject/src/com/tbf/PortfolioReport.java
@@ -13,20 +13,23 @@ public class PortfolioReport {
 				"Portfolio", "Owner", "Manager", "Fees", "Commisions", "Weighted Risk", "Return", "Total"));
 		
 		
-		
+		double fee = 0;
+		double commi = 0;
+		double theReturn = 0;
+		double finalTotal = 0;
 		for (Portfolio i : portList) {
-			report.append(String.format("%-8s %-20s %-20s %-20.2f $%12.2f %12.4f $%12.2f $%12.2f\n", 
+			report.append(String.format("%-8s %-20s %-20s %-20.2f $%12.2f %12.6f $%12.2f $%12.2f\n", 
 					i.getPortCode(), i.getOwnerName(), i.getManagerName(), i.getFee(), i.getCommission(), 
-					i.getWeightedRisk(), i.getReturn(), i.getTotal())); 
-			
+					i.getWeightedRisk(), i.getReturn(), i.getTotal()));
+			fee += i.getFee();
+			commi += i.getCommission();
+			theReturn += i.getReturn();
+			finalTotal += i.getTotal();
 		}
 		System.out.println(report);
-		System.out.println("\t\t-------------------------------------------------");
-//		for(Portfolio j : portList) {
-//			System.out.printf("\t\t\t %15.2f %12.2f %12.2f %12.2f", j.getFeeTotal(), j.getCommiTotal(), j.getReturnTotal(),j.getTotalTotal());
-//		}
-		//hello
+		System.out.println(fee+"   "+commi+"   "+theReturn+"   "+finalTotal);
+		//System.out.println("\t\t\t %15.2f %12.2f %12.2f %12.2f", getFeeTotal(), getCommiTotal(), getReturnTotal(), getTotalTotal());
+		}
 	}
-}
 
 
