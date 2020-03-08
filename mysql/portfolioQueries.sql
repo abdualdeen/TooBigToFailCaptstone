94cffbe0eebe0b84d2dbea7217104c075def5dfc
hbalandran <59933286+hbalandran@users.noreply.github.com>
2020-03-08 12:57:08 -0500

    test cases
    If you could check over the email part that would be great. Not 100% sure i did it correctly

--- mysql/portfolioQueries.sql
+++ mysql/portfolioQueries.sql
@@ -2,17 +2,27 @@
 -- use hbalandran;
 
 -- address
-insert into Address(street, city, state, zip, country) values ("123 Elmo Ln", "Barnes", "CA", "12345", "USA");
-insert into Address(street, city, state, zip, country) values ("456 Cookie St", "Bird", "MI", "67890", "USA");
-insert into Address(street, city, state, zip, country) values ("789  Big Bird Blvd", "Micky", "FL", "11234", "USA");
+insert into Address(street, city, state, zip, country) values ("420 Grove St", "Los Santos", "CA", "91111", "USA");
+insert into Address(street, city, state, zip, country) values ("100 Wall St", "New York", "NY", "10005-0012", "USA");
+insert into Address(street, city, state, zip, country) values ("69 Shinjiku St", "Tokyo", "Shinjiku", "", "Japan");
+insert into Address(street, city, state, zip, country) values ("892 Momona St", "Honolulu", "HI", "96820", "USA");
+insert into Address(street, city, state, zip, country) values ("12 Pol St", "Los Angeles", "CA", "12345", "USA");
+insert into Address(street, city, state, zip, country) values ("101 N 14th St", "Lincoln", "NE", "68508", "USA");
+insert into Address(street, city, state, zip, country) values ("123 Alphabet Blvd", "Las Vegas", "NV", "12345", "USA");
 
 -- person
-insert into Person(alphaCode, brokerStat, lastName, firstName) values ("123a", "E", "John", "Jimmy");
-insert into Person(alphaCode, lastName, firstName) values ("456b", "Queen", "Dairy");
-insert into Person(alphaCode, brokerStat, lastName, firstName) values ("789c", "J", "King", "Burger");
+insert into Person(alphaCode, lastName, firstName) values ("944c", "Johnson", "Carl");
+insert into Person(alphaCode, brokerStat, lastName, firstName) values ("aef1", "E, sec001",  "White", "Walter");
+insert into Person(alphaCode, lastName, firstName) values ("ma12", "Yagami", "Light");
+insert into Person(alphaCode, lastName, firstName) values ("1svndr", "McLovin", "Fogell");
+insert into Person(alphaCode, brokerStat, lastName, firstName) values ("231", "E, sec221", "Slater", "Officer");
+insert into Person(alphaCode, lastName, firstName) values ("wrddoc", "John", "Jimmy");
+insert into Person(alphaCode, lastName, firstName) values ("1svndr", "Jammy", "Jenny");
 
 -- email
-insert into EmailAddress (personId, emailAddress) values (1, "123aJimmyJohn@gmail.com");
-insert into EmailAddress (personId, emailAddress) values (1, "Jimmy_Jammy_Johns@outlook.com");
-insert into EmailAddress (personId, emailAddress) values (2, "Dairy.Queens.King456@hotmail.com");
+insert into EmailAddress (personId, emailAddress) values (1, "cj@cjcluckinbell.com");
+insert into EmailAddress (personId, emailAddress) values (3, "yagamiL@tokyopolice.com");
+insert into EmailAddress (personId, emailAddress) values (5, "12.O.Slater@gmail.com, Donut.Slater@hotmail.com");
+insert into EmailAddress (personId, emailAddress) values (6, "jimmy_jammy_johns@gmail.com, jimmy.tomy.jammy@outlook.com");
+insert into EmailAddress (personId, emailAddress) values (7, "better_sammies@hotmail.com, Jenny_Jammy12@outlook.com");
 
