e38acadd7723c1e4d12194f424334d943db5b73a
hbalandran <59933286+hbalandran@users.noreply.github.com>
2020-03-10 14:35:10 -0500

    kwaf
    12,13,14,15 are done. test cases are in DB, portCode and assetCode have been added.

--- mysql/PortfolioDB.sql
+++ mysql/PortfolioDB.sql
@@ -1,7 +1,7 @@
 -- create database ahamad;
 -- use ahamad;
 -- use hbalandran;
-drop table if exists PortfolioAsset;
+drop table if exists PortfolioAssets;
 drop table if exists Portfolio;
 drop table if exists EmailAddress;
 drop table if exists Asset;
@@ -46,19 +46,78 @@ create table if not exists Asset (
   stockSymb varchar(5),
   sharePrice double,
   omega double,
-  investmentValue double
+  investmentValue double,
+  assetCode varchar(100) not null
 );  
   
 create table if not exists Portfolio (
   portId int primary key not null auto_increment,
   ownerId int not null, foreign key (ownerId) references Person(personId),
   managerId int not null, foreign key (managerId) references Person(personId),
-  benefId int, foreign key (benefId) references Person(personId)
+  benefId int, foreign key (benefId) references Person(personId),
+  portCode varchar(100) not null
   );
 
-create table if not exists PortfolioAsset (
+create table if not exists PortfolioAssets (
   portAssetId int primary key not null auto_increment,
   portId int not null, foreign key (portId) references Portfolio(portId),
   assetId int not null, foreign key (assetId) references Asset(assetId),
   assetInfo double not null
 );
+
+
+-- address
+insert into Address (street, city, state, zip, country) values ("420 Grove St", "Los Santos", "CA", "91111", "USA");
+insert into Address (street, city, state, zip, country) values ("100 Wall St", "New York", "NY", "10005-0012", "USA");
+insert into Address (street, city, state, zip, country) values ("69 Shinjiku St", "Tokyo", "Shinjiku", "", "Japan");
+insert into Address (street, city, state, zip, country) values ("892 Momona St", "Honolulu", "HI", "96820", "USA");
+insert into Address (street, city, state, zip, country) values ("12 Pol St", "Los Angeles", "CA", "12345", "USA");
+insert into Address (street, city, state, zip, country) values ("101 N 14th St", "Lincoln", "NE", "68508", "USA");
+insert into Address (street, city, state, zip, country) values ("123 Alphabet Blvd", "Las Vegas", "NV", "12345", "USA");
+insert into Address (street, city, state, country) values ("99 Algebra St", "Khwarazm", "Aral Sea", "Persia");
+
+-- person
+insert into Person (alphaCode, lastName, firstName, addressId) values ("944c", "Johnson", "Carl", 1);
+insert into Person (alphaCode, brokerStat, lastName, firstName, addressId) values ("aef1", "E, sec001",  "White", "Walter", 2);
+insert into Person (alphaCode, lastName, firstName, addressId) values ("ma12", "Yagami", "Light", 3);
+insert into Person (alphaCode, lastName, firstName, addressId) values ("1svndr", "McLovin", "Fogell", 4);
+insert into Person (alphaCode, brokerStat, lastName, firstName, addressId) values ("231", "E, sec221", "Slater", "Officer", 5);
+insert into Person (alphaCode, lastName, firstName, addressId) values ("wrddoc", "John", "Jimmy", 6);
+insert into Person (alphaCode, lastName, firstName, addressId) values ("1svndr", "Jammy", "Jenny", 7);
+insert into Person (alphaCode, lastName, firstName, addressId) values ("sqrt", "Al-Kawarzimi", "Muhammad", 8);
+
+-- email
+insert into EmailAddress (personId, emailAddress) values (1, "cj@cjcluckinbell.com");
+insert into EmailAddress (personId, emailAddress) values (3, "yagamiL@tokyopolice.com");
+insert into EmailAddress (personId, emailAddress) values (5, "12.O.Slater@gmail.com");
+insert into EmailAddress (personId, emailAddress) values (5, "Donut.Slater@hotmail.com");
+insert into EmailAddress (personId, emailAddress) values (6, "jimmy_jammy_johns@gmail.com");
+insert into EmailAddress (personId, emailAddress) values (6, "jimmy.tomy.jammy@outlook.com");
+insert into EmailAddress (personId, emailAddress) values (7, "better_sammies@hotmail.com");
+insert into EmailAddress (personId, emailAddress) values (7, "Jenny_Jammy12@outlook.com");
+insert into EmailAddress (personId, emailAddress) values (8, "mkawarzimi@algebra.com");
+
+
+-- Perosn 1 Assets
+insert into Asset (quartDivi, BaseROR, omega, investmentValue, label, assetType, assetCode) values (35000.0, 0.03, 0.32, 1212500.0, "Cluckin Bell restaurant chain", "P", "FOOD12");
+insert into Asset (apr, label, assetType, assetCode) values (0.25, "G Savings Account", "D", "AGTSAV");
+insert into Asset (quartDivi, BaseROR, beta, stockSymb, sharePrice, label, assetType, assetCode) values (10, 0.025, .13, "LDEG", 106.13, "Linux Development Group", "S", "B0oWM");
+
+-- Person 2 Assets
+-- none.
+
+-- Person 3 Assets
+insert into Asset (quartDivi, BaseROR, beta, stockSymb, sharePrice, label, assetType, assetCode) values (5.45, 0.031, .075, "KO", 41.08, "Coca-cola", "S", "321CC");
+
+-- Portfolios 
+insert into Portfolio (ownerId, managerId, benefId, portCode) values (1, 2, 3,"PT001");
+insert into Portfolio (ownerId, managerId, portCode) values (4, 5, "PF006");
+insert into Portfolio (ownerId, managerId, benefId, portCode) values (6, 7, 8, "PF002");
+
+-- (Person 1 port) Portfolio Asset association table with asset info. 
+insert into PortfolioAssets (portId, assetId, assetInfo) values (1, 1, 105);
+insert into PortfolioAssets (portId, assetId, assetInfo) values (1, 2, 26534.21);
+insert into PortfolioAssets (portId, assetId, assetInfo) values (1, 3, 125);
+
+-- (person 3 port) 
+insert into PortfolioAssets (portId, assetId, assetInfo) values (2, 4, 25.5);
--- mysql/portfolioQueries.sql
+++ mysql/portfolioQueries.sql
@@ -1,62 +1,6 @@
 -- use ahamad;
 -- use hbalandran;
 
--- address
-insert into Address (street, city, state, zip, country) values ("420 Grove St", "Los Santos", "CA", "91111", "USA");
-insert into Address (street, city, state, zip, country) values ("100 Wall St", "New York", "NY", "10005-0012", "USA");
-insert into Address (street, city, state, zip, country) values ("69 Shinjiku St", "Tokyo", "Shinjiku", "", "Japan");
-insert into Address (street, city, state, zip, country) values ("892 Momona St", "Honolulu", "HI", "96820", "USA");
-insert into Address (street, city, state, zip, country) values ("12 Pol St", "Los Angeles", "CA", "12345", "USA");
-insert into Address (street, city, state, zip, country) values ("101 N 14th St", "Lincoln", "NE", "68508", "USA");
-insert into Address (street, city, state, zip, country) values ("123 Alphabet Blvd", "Las Vegas", "NV", "12345", "USA");
-insert into Address (street, city, state, country) values ("99 Algebra St", "Khwarazm", "Aral Sea", "Persia");
-
--- person
-insert into Person (alphaCode, lastName, firstName, addressId) values ("944c", "Johnson", "Carl", 1);
-insert into Person (alphaCode, brokerStat, lastName, firstName, addressId) values ("aef1", "E, sec001",  "White", "Walter", 2);
-insert into Person (alphaCode, lastName, firstName, addressId) values ("ma12", "Yagami", "Light", 3);
-insert into Person (alphaCode, lastName, firstName, addressId) values ("1svndr", "McLovin", "Fogell", 4);
-insert into Person (alphaCode, brokerStat, lastName, firstName, addressId) values ("231", "E, sec221", "Slater", "Officer", 5);
-insert into Person (alphaCode, lastName, firstName, addressId) values ("wrddoc", "John", "Jimmy", 6);
-insert into Person (alphaCode, lastName, firstName, addressId) values ("1svndr", "Jammy", "Jenny", 7);
-insert into Person (alphaCode, lastName, firstName, addressId) values ("sqrt", "Al-Kawarzimi", "Muhammad", 8);
-
--- email
-insert into EmailAddress (personId, emailAddress) values (1, "cj@cjcluckinbell.com");
-insert into EmailAddress (personId, emailAddress) values (3, "yagamiL@tokyopolice.com");
-insert into EmailAddress (personId, emailAddress) values (5, "12.O.Slater@gmail.com");
-insert into EmailAddress (personId, emailAddress) values (5, "Donut.Slater@hotmail.com");
-insert into EmailAddress (personId, emailAddress) values (6, "jimmy_jammy_johns@gmail.com");
-insert into EmailAddress (personId, emailAddress) values (6, "jimmy.tomy.jammy@outlook.com");
-insert into EmailAddress (personId, emailAddress) values (7, "better_sammies@hotmail.com");
-insert into EmailAddress (personId, emailAddress) values (7, "Jenny_Jammy12@outlook.com");
-insert into EmailAddress (personId, emailAddress) values (8, "mkawarzimi@algebra.com");
-
-
--- Perosn 1 Assets
-insert into Asset (quartDivi, BaseROR, omega, investmentValue, label, assetType) values (35000.0, 0.03, 0.32, 1212500.0, "Cluckin Bell restaurant chain", "P");
-insert into Asset (apr, label, assetType) values (0.25, "G Savings Account", "D");
-insert into Asset (quartDivi, BaseROR, beta, stockSymb, sharePrice, label, assetType) values (10, 0.025, .13, "LDEG", 106.13, "Linux Development Group", "S");
-
--- Person 2 Assets
--- none.
-
--- Person 3 Assets
-insert into Asset (quartDivi, BaseROR, beta, stockSymb, sharePrice, label, assetType) values (5.45, 0.031, .075, "KO", 41.08, "Coca-cola", "S");
-
--- Portfolios 
-insert into Portfolio (ownerId, managerId, benefId) values (1, 2, 3);
-insert into Portfolio (ownerId, managerId) values (4, 5);
-insert into Portfolio (ownerId, managerId, benefId) values (6, 7, 8);
-
--- (Person 1 port) Portfolio Asset association table with asset info. 
-insert into PortfolioAsset (portId, assetId, assetInfo) values (1, 1, 35);
-insert into PortfolioAsset (portId, assetId, assetInfo) values (1, 2, 26534.21);
-insert into PortfolioAsset (portId, assetId, assetInfo) values (1, 3, 125);
-
--- (person 3 port) 
-insert into PortfolioAsset (portId, assetId, assetInfo) values (2, 4, 25.5);
-
 -- ==Queries== --
 -- 1
 select * from Person;
@@ -75,7 +19,7 @@ update EmailAddress set emailAddress = "cj@grovestreet.com" where emailAddressId
 delete from EmailAddress where personId = 8;
 update Portfolio set benefId = null where portId = 3;
 delete from Person where personId = 8;
-delete from Address where addressId = 8;
+-- delete from Address where addressId = 8;
 
 -- 6
 insert into Address (street, city, state, country) values ("99 Algebra St", "Khwarazm", "Aral Sea", "Persia");
@@ -83,70 +27,10 @@ insert into Person(alphaCode, lastName, firstName, addressId) values ("sqrt", "A
 insert into EmailAddress (personId, emailAddress) values (9, "mkawarzimi@algebra.com");
 
 -- 7 
+select * from Asset a join PortfolioAssets pa on a.assetId = pa.assetId join Portfolio p on p.portId = pa.portId;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- 8 
+select * from Asset a join PortfolioAssets pa on a.assetId = pa.assetId join Portfolio p on p.portId = pa.portId join Person pe on pe.personId = p.ownerId;
 
 -- 9
 -- select * from Asset;
@@ -156,15 +40,28 @@ insert into Asset (quartDivi, BaseROR, omega, investmentValue, label, assetType)
 insert into Portfolio (ownerId, managerId) values (9, 1);
 
 -- 11
-insert into PortfolioAsset (portId, assetId, assetInfo) values (4, 5, 9999);
+insert into PortfolioAssets (portId, assetId, assetInfo) values (1, 5, 9999);
 
 -- 12
 -- select * from Portfolio;
-select pe.lastName, count(pa.assetId) as numberOfAssets from PortfolioAsset pa
-  left join Portfolio p on p.portId = pa.portId
-  left join Person pe on pe.personId = p.ownerId;
+select pe.lastName, count(pa.assetId) as numberOfAssets from PortfolioAssets pa
+  join Portfolio p on p.portId = pa.portId
+  right join Person pe on pe.personId = p.ownerId group by pe.personId;
 -- select p.lastName, count(a.assetId) as numberOfAssets from Asset a
 --   left join PortfolioAsset pa on a.assetId  = pa.assetId
 --   left join Portfolio po on po.portId = pa.portId
 --   left join Person p on p.personId = ownerId;
 -- select count(a.assetId) as numberOfAssets from Asset a where a.assetId
+
+-- 13
+select pe.lastName, count(p.portId) as numberOfAssets from Portfolio p
+  right join Person pe on pe.personId = p.managerId group by pe.personId;
+  
+-- 14
+select p.portCode, sum(a.sharePrice*pa.assetInfo) as sumOfAssets from PortfolioAssets pa
+  join Portfolio p on p.portId = pa.portId
+  join Asset a on a.assetId = pa.assetId where a.assetType = "S" group by p.portId;
+  
+-- 15
+select a.label, sum(pa.assetInfo) as sumOfStakePercentage from PortfolioAssets pa
+  join Asset a on pa.assetId = a.assetId where (a.assetType = "P") group by a.assetId having sumOfStakePercentage>100;
