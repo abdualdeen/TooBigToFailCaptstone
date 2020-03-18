9738b60715a393d9fa05dbcf9dbbec3d7a908ab3
ahamad2 <ahamad2@unl.edu>
2020-03-17 23:39:32 -0500

    changes to the sql test  queries file

--- mysql/assignmentTestQueries.sql
+++ mysql/assignmentTestQueries.sql
@@ -5,3 +5,12 @@ update Portfolio set benefId = null where portId = + personCode +;
 delete from Person where personId = + personCode +;
 
 -- Removing all persons.
+
+-- adding email to existing person
+
+
+-- Adding a new person
+
+insert into Address (street, city, stateId, countryId) values ("99 Algebra St", "Khwarazm", 7, 3);
+insert into Person(alphaCode, lastName, firstName, addressId) values ("sqrt", "Al-Kawarzimi", "Muhammad", 9);
+insert into EmailAddress (personId, emailAddress) values (9, "mkawarzimi@algebra.com");
