a2692678121fc1cb3bbddfc97c6012560aaf3226
ahamad2 <ahamad2@unl.edu>
2020-03-19 22:49:43 -0500

    last update for the night

--- mysql/assignmentTestQueries.sql
+++ mysql/assignmentTestQueries.sql
@@ -1,8 +1,8 @@
 -- ==Queries for the assignment== --
 -- Removing a person by personCode
-delete from EmailAddress where personId = + personCode +;
-update Portfolio set benefId = null where portId = + personCode +;
-delete from Person where personId = + personCode +;
+-- delete from EmailAddress where personId = + personCode +;
+-- update Portfolio set benefId = null where portId = + personCode +;
+-- delete from Person where personId = + personCode +;
 
 -- Removing all persons.
 
@@ -14,3 +14,6 @@ delete from Person where personId = + personCode +;
 insert into Address (street, city, stateId, countryId) values ("99 Algebra St", "Khwarazm", 7, 3);
 insert into Person(alphaCode, lastName, firstName, addressId) values ("sqrt", "Al-Kawarzimi", "Muhammad", 9);
 insert into EmailAddress (personId, emailAddress) values (9, "mkawarzimi@algebra.com");
+
+-- for all assets.
+select * from Asset;
