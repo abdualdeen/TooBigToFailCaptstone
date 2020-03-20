14f5171ffdfd604e37ca4d3aab4583c812def282
ahamad2 <ahamad2@unl.edu>
2020-03-19 20:00:15 -0500

    moved method over to person class.

--- semesterProject/src/com/tbf/DBTool.java
+++ semesterProject/src/com/tbf/DBTool.java
@@ -5,8 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class DBTool {
 
@@ -30,36 +29,6 @@ public class DBTool {
 		return conn;
 	}
 
-	public static List<Person> retrieveAllPerson() {
-		Connection conn = connectToDB();
-		String query = "Select p.personId, p.alphaCode, p.brokerStat, p.lastName, p.firstName, a.street, a.city, a.zip, s.abbreviation, c.name from Person p"
-				+ " join Address a on p.addressId = a.addressId join State s on a.stateId = s.stateId join Country c on a.countryId = c.countryId;";
-
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		
-		List<Person> persons = new ArrayList<>();
-		
-			ps = conn.prepareStatement(query);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				int personId = rs.getInt("personId");
-				int alphaCode = rs.getInt("alphaCode");
-				String brokerStat = rs.getString("brokerStat");
-				String lastName = rs.getString("lastName");
-				String firstName = rs.getString("firstName");
-				String street = rs.getString("street");
-				String city = rs.getString("city");
-				String zip = rs.getString("zip");
-				String abbreviation = rs.getString("abbreviation");
-				String name = rs.getString("name");
-				Name n = new Name(firstName, lastName);
-				Address a = new Address(street, city, abbreviation, zip, name);
-				Person p = new Person(personId, alphaCode, brokerStat, n, a);
-				persons.add(p);
-			}
-				return persons;
-	}
 
 	public static void disconnectFromDB(Connection conn, PreparedStatement ps, ResultSet rs) {
 		try {
