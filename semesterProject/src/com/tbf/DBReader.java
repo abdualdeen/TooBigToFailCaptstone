2b0401100603718340ca0a2ace6f9281cc40ef78
ahamad2 <ahamad2@unl.edu>
2020-03-20 22:30:59 -0500

    retrieveassets fixed.

--- semesterProject/src/com/tbf/DBReader.java
+++ semesterProject/src/com/tbf/DBReader.java
@@ -47,10 +47,11 @@ public class DBReader {
 		DBTool.disconnectFromDB(conn, ps, rs);
 		return emails;
 	}
-	public static Person retrievePerson(int int1){
+	
+	public static Person retrievePerson(int personId){
 		Person person = new Person();
 		Connection conn = DBTool.connectToDB();
-		String query = "select * from Person where personId = " + int1 + ";";
+		String query = "select * from Person where personId = " + personId + ";";
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
@@ -65,49 +66,10 @@ public class DBReader {
 		DBTool.disconnectFromDB(conn, ps, rs);
 		return person;
 	}
-	public static List<Asset> retrieveAssets(int portId) {
-		List<Asset> assets = new ArrayList<>();
-		Connection conn = DBTool.connectToDB();
-		String query = "select * from Asset where assetId = " + portId + ";";
-		
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		
-		try {
-			ps = conn.prepareStatement(query);
-			rs = ps.executeQuery();
-			
-			while (rs.next()) {
-				if (rs.getString("assetType").equals("D")) {
-					Asset newDepo  = new DepositAsset(rs.getString("assetCode"), rs.getString("assetType"), rs.getString("label"), rs.getDouble("apr"));
-					assets.add(newDepo);
-
-				} else if (rs.getString("assetType").equals("S")) {
-					Asset newStock = new Stock(rs.getString("assetCode"), rs.getString("assetType"), rs.getString("label"), rs.getDouble("quartDivi"), rs.getDouble("baseROR"),
-							rs.getDouble("beta"), rs.getString("stockSymb"), rs.getDouble("SharePrice"));
-					assets.add(newStock);
-
-					
-				} else if (rs.getString("assetType").equals("P")) {
-					Asset newPI = new PrivateInvest(rs.getString("assetCode"), rs.getString("assetType"), rs.getString("label"), rs.getDouble("quartDivi"), 
-							rs.getDouble("baseROR"), rs.getDouble("omega"), rs.getDouble("investmentValue"));
-					assets.add(newPI);
-
-				}
- 			}
-		} catch (SQLException sqle) {
-			throw new RuntimeException(sqle);
-		}
-		
-		DBTool.disconnectFromDB(conn, ps, rs);
-		return assets;
-	}
+	
 	
 	public static List<Person> retrieveAllPerson() {
 		Connection conn = DBTool.connectToDB();
-//		String query = "Select p.personId, p.alphaCode, p.brokerStat, p.lastName, p.firstName, a.street, a.city, a.zip, s.abbreviation, c.name, e.emailAddress from Person p"
-//				+ " join Address a on p.addressId = a.addressId join State s on a.stateId = s.stateId join Country c on a.countryId = c.countryId join EmailAddress e"
-//				+ " on p.personId = e.personId;";
 		String query = "select * from Person;";
 		PreparedStatement ps = null;
 		ResultSet rs = null;
@@ -118,27 +80,7 @@ public class DBReader {
 				ps = conn.prepareStatement(query);
 				rs = ps.executeQuery();
 				while (rs.next()) {
-//					String alphaCode = rs.getString("alphaCode");
-//					String brokerStat = rs.getString("brokerStat");
-//					String lastName = rs.getString("lastName");
-//					String firstName = rs.getString("firstName");
-//					String street = rs.getString("street");
-//					String city = rs.getString("city");
-//					String zip = rs.getString("zip");
-//					String abbreviation = rs.getString("abbreviation");
-//					String name = rs.getString("name");
-//					String emailAddress = 
-					
-					//=====
-					// For testing
-//					DBTool.disconnectFromDB(conn, ps, rs);
-//					break;
-					//++++ delete once done
-							
-//					ArrayList<String> email = retrieveEmailAddress(rs.getInt("personId"));
-//					email.add(emailAddress);
 					Name n = new Name(rs.getString("firstName"), rs.getString("lastName"));
-//					Address a = new Address(rs.getString("street"), rs.getString("city"), rs.getString("abbreviation"), rs.getString("zip"), rs.getString("name"));
 					Person p = new Person(rs.getString("alphaCode"), rs.getString("brokerStat"), n, retrieveAddress(rs.getInt("addressId")), 
 							retrieveEmailAddress(rs.getInt("personId")));
 					persons.add(p);
@@ -189,6 +131,45 @@ public class DBReader {
 		return assets;
 	}
 
+	public static List<Asset> retrieveAssets(int portId) {
+		List<Asset> assets = new ArrayList<>();
+		Connection conn = DBTool.connectToDB();
+		String query = "select * from Asset a join PortfolioAsset pa on a.assetId = pa.assetId where pa.portId = " + portId + ";";
+		
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			ps = conn.prepareStatement(query);
+			rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				if (rs.getString("assetType").equals("D")) {
+					Asset newDepo  = new DepositAsset(rs.getString("assetCode"), rs.getString("assetType"), rs.getString("label"), rs.getDouble("apr"), 
+							rs.getDouble("assetInfo"));
+					assets.add(newDepo);
+
+				} else if (rs.getString("assetType").equals("S")) {
+					Asset newStock = new Stock(rs.getString("assetCode"), rs.getString("assetType"), rs.getString("label"), rs.getDouble("quartDivi"), rs.getDouble("baseROR"),
+							rs.getDouble("beta"), rs.getString("stockSymb"), rs.getDouble("SharePrice"), rs.getDouble("assetInfo"));
+					assets.add(newStock);
+
+					
+				} else if (rs.getString("assetType").equals("P")) {
+					Asset newPI = new PrivateInvest(rs.getString("assetCode"), rs.getString("assetType"), rs.getString("label"), rs.getDouble("quartDivi"), 
+							rs.getDouble("baseROR"), rs.getDouble("omega"), rs.getDouble("investmentValue"), rs.getDouble("assetInfo"));
+					assets.add(newPI);
+
+				}
+ 			}
+		} catch (SQLException sqle) {
+			throw new RuntimeException(sqle);
+		}
+		
+		DBTool.disconnectFromDB(conn, ps, rs);
+		return assets;
+	}
+	
 	public static List<Portfolio> retrieveAllPortfolios() {
 		Connection conn = DBTool.connectToDB();
 		String query  = "Select * from Portfolio;";
@@ -202,7 +183,9 @@ public class DBReader {
 			ps = conn.prepareStatement(query);
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				Portfolio p = new Portfolio(rs.getString("portCode"), retrievePerson(rs.getInt("ownerId")), retrievePerson(rs.getInt("managerId")), retrievePerson(rs.getInt("beneficiaryId")), retrieveAssets(rs.getInt("portId"))); 
+				Portfolio p = new Portfolio(rs.getString("portCode"), retrievePerson(rs.getInt("ownerId")), retrievePerson(rs.getInt("managerId")), 
+						retrievePerson(rs.getInt("beneficiaryId")), retrieveAssets(rs.getInt("portId"))); 
+				portfolios.add(p);
 			}
 		}
 			catch (SQLException sqle) {
